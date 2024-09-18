b0ea5a64114e125b3d9dcd6c259e617675b5c87d
huaizhic <huaizhihuaizhi@gmail.com>
2024-09-18 22:02:58 +0800

    Removing node modules folder from github (gitignore)

--- server/supabase.js
+++ server/supabase.js
@@ -7,8 +7,8 @@ const supabaseObject = require('@supabase/supabase-js');
 // // Supabase public API key, okay to expose if proper RLS policies in place
 // const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 
-const supabaseUrl = '';
-const supabaseKey = '';
+const supabaseUrl = 'https://fhdsmgbcnjzqakudimet.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoZHNtZ2Jjbmp6cWFrdWRpbWV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUzNDcxOTksImV4cCI6MjA0MDkyMzE5OX0.q7UtKFSdgrXUPCr1sFSAlKbkRdi8dp6TyGbQ3yBvK8w';
 const supabase = supabaseObject.createClient(supabaseUrl, supabaseKey);
 
 // console.log(supabase);
